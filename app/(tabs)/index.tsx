41b9fc753c0f07f07a3bfdfd6bbabe45ac9960da
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2024-03-04 18:38:17 +0300

    Updated main page, colors & updated packages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { NativeScrollEvent, NativeSyntheticEvent, ScrollView, StyleSheet } from "react-native";
+import { Text, View } from "@/components/Themed";
+import React from "react";
+import { AnimatedFAB } from "react-native-paper";
 
 export default function TabOneScreen() {
+  const [isExtended, setIsExtended] = React.useState(true);
+
+  const onScroll = ({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const currentScrollPosition = Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+
+    setIsExtended(currentScrollPosition <= 0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <ScrollView onScroll={onScroll}>
+        {[...new Array(100).keys()].map((_, i) => (
+          <Text>{i}</Text>
+        ))}
+      </ScrollView>
+      <AnimatedFAB
+        icon="plus"
+        label="Добавить"
+        extended={isExtended}
+        onPress={() => undefined}
+        animateFrom="right"
+        iconMode="dynamic"
+        style={[styles.fabStyle]}
+      />
     </View>
   );
 }
@@ -16,16 +35,21 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  fabStyle: {
+    bottom: 16,
+    right: 16,
+    position: "absolute",
   },
 });
