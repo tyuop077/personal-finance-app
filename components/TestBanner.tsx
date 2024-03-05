4bc09254e7bf27f08da00935a6a62e3179f4e12e
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2024-03-05 22:52:19 +0300

    Fixed ios issues

--- components/TestBanner.tsx
+++ components/TestBanner.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     marginBottom: 85,
-    ...(Platform.OS === "web" ? { pointerEvents: "none" } : {}),
+    ...(Platform.OS !== "android" ? { pointerEvents: "none" } : {}),
   },
 });
 
