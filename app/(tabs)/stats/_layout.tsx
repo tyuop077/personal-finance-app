0bff9aaa1fbad39dc9b1deec9ca393a227100e08
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2024-04-02 00:32:47 +0300

    Migrated from state to store & moved add modal child inside

--- app/(tabs)/stats/_layout.tsx
+++ app/(tabs)/stats/_layout.tsx
@@ -6,6 +6,8 @@ import { useRootStore } from "@/hooks/useRootStore";
 import { Tabs, TabScreen, TabsProvider } from "react-native-paper-tabs";
 import Revenues from "@/app/(tabs)/stats/revenues";
 import Expenses from "@/app/(tabs)/stats/expenses";
+import defaultFinance from "@/mock/defaultFinance";
+import { runInAction } from "mobx";
 
 export default function StatsPage() {
   const { finances } = useRootStore();
@@ -25,8 +27,19 @@ export default function StatsPage() {
   };
 
   const handleReset = () => {
-    finances.financeRepository.removeAll();
-    finances.financeModel.items = [];
+    runInAction(() => {
+      finances.financeRepository.removeAll();
+      finances.financeModel.items = [];
+    });
+    alert("Перезапустите для обновления");
+  };
+
+  const handleResetDefault = () => {
+    runInAction(() => {
+      finances.financeRepository.setItems(defaultFinance);
+      finances.financeModel.items = defaultFinance;
+    });
+    alert("Перезапустите для обновления");
   };
 
   return (
@@ -38,7 +51,8 @@ export default function StatsPage() {
           onDismiss={handleCloseMenu}
           anchor={<Appbar.Action icon="feather/more-vertical" onPress={handleOpenMenu} />}
         >
-          <Menu.Item title="Сбросить состояние" onPress={handleReset} />
+          <Menu.Item title="Сбросить до пустого" onPress={handleReset} />
+          <Menu.Item title="Сбросить до тестового" onPress={handleResetDefault} />
           <Menu.Item title="Настройки" onPress={handleSettings} />
         </Menu>
       </Appbar.Header>
