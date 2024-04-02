87b3778cd98c61a1a6864e4cd1654ffe5afc73a5
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2024-04-02 05:57:11 +0300

    Updated theme settings page

--- app/settings/theme.tsx
+++ app/settings/theme.tsx
@@ -1,28 +1,33 @@
 import { ScrollView, StyleSheet } from "react-native";
-import { Appbar, List } from "react-native-paper";
-import { router } from "expo-router";
+import { List, useTheme } from "react-native-paper";
 import { useContext } from "react";
 import { PreferencesContext, ThemeState } from "@/modules/theme/theme.context";
 
 export default function SettingsPage() {
   const { toggleTheme, themeState } = useContext(PreferencesContext);
+  const theme = useTheme();
+
+  const buttonStyle = (state: ThemeState) => (themeState === state ? { color: theme.colors.primary } : {});
 
   return (
     <ScrollView style={styles.container}>
       <List.Item
-        title="Автоматически"
+        title="Как на устройстве"
         onPress={() => toggleTheme(ThemeState.AUTOMATIC)}
         left={() => <List.Icon icon="palette" />}
+        titleStyle={buttonStyle(ThemeState.AUTOMATIC)}
       />
       <List.Item
         title="Светлая"
         onPress={() => toggleTheme(ThemeState.LIGHT)}
         left={() => <List.Icon icon="palette" />}
+        titleStyle={buttonStyle(ThemeState.LIGHT)}
       />
       <List.Item
         title="Тёмная"
         onPress={() => toggleTheme(ThemeState.DARK)}
         left={() => <List.Icon icon="palette" />}
+        titleStyle={buttonStyle(ThemeState.DARK)}
       />
     </ScrollView>
   );
