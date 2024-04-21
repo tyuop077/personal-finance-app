240187186ea26496a123b2d3aaabc0f049921bf8
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2024-04-21 23:27:19 +0300

    Made working current balance counter

--- components/FinanceCard.tsx
+++ components/FinanceCard.tsx
@@ -1,15 +1,21 @@
 import { StyleSheet, View } from "react-native";
 import { Text, useTheme } from "react-native-paper";
 import React from "react";
+import { useRootStore } from "@/hooks/useRootStore";
 
 export const FinanceCard = () => {
   const theme = useTheme();
+  const { finances } = useRootStore();
 
   return (
     <View style={styles.financeContainer}>
       <View style={[styles.balanceContainer, { backgroundColor: theme.colors.secondaryContainer }]}>
         <Text style={[styles.balanceTitle, { color: theme.colors.onSecondaryContainer }]}>Баланс</Text>
-        <Text style={[styles.balanceValue, { color: theme.colors.onSecondaryContainer }]}>50000</Text>
+        <Text style={[styles.balanceValue, { color: theme.colors.onSecondaryContainer }]}>
+          {finances.financeModel.items
+            .reduce((acc, i) => acc + i.value, 0)
+            .toLocaleString("ru-RU", { style: "currency", currency: "RUB" })}
+        </Text>
       </View>
     </View>
   );
